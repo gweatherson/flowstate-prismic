87d9a96053aaf396fa91549e4c1a13b47c9a7f31
Glenn Weatherson <glenn@970design.com>
2023-11-10 19:06:54 -0800

    add meta data from prismic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-      <h1 className="font-display text-5xl">hello nerds</h1>
+      <h1 className="font-display text-5xl">hello nerds...</h1>
     </main>
   )
 }
+
